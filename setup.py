3724b9e31f9cde49c06dd61a78af170dd023f9a3
omerholz <omer.holz@gmail.com>
2017-03-31 11:44:31 -0400

    up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="myconfig",
-    version="0.0.1",
+    version="0.0.2",
     description='a config wrapper that does what I want',
     url='https://github.com/omerholz/myconfig',
     zip_safe=False,
